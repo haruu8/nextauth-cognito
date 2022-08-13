8d1dc89ed5f7f5a96e1aad093f89e3a4c502a4c9
haru <haru.contact.me@gmail.com>
2022-08-14 03:44:56 +0900

    make func that we can get accessToken

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,13 +3,12 @@ import { signIn, signOut, useSession } from 'next-auth/react';
 
 const Home: NextPage = () => {
   const { data: session } = useSession();
-  const { accessToken } = session;
 
   if (session && session.user) {
     return (
       <>
         Signed in as {session.user.email} <br />
-        access token: {accessToken}
+        Access Token: {session.accessToken} <br />
         <button onClick={() => signOut()}>Sign out</button>
       </>
     );
